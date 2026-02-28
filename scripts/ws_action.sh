61d6ea5c727336721a323198a7c617dd6c6dabfa
KR <michal.nowicki1@code.com>
2026-02-28 15:39:18 +0100

    complete monitor-workspace overhaul

--- scripts/ws_action.sh
+++ scripts/ws_action.sh
@@ -3,20 +3,17 @@
 ACTION=$1
 BASE_NUM=$2
 
-# Get the currently focused workspace number
 CURRENT_WS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).num')
 
-# If we are currently on the second monitor (workspaces 11-19), add 10 to our target
+# Generate the NUMBER:NAME format (e.g., 15:5 or 5:5)
 if [ "$CURRENT_WS" -ge 11 ] && [ "$CURRENT_WS" -le 19 ]; then
-  TARGET_WS=$((BASE_NUM + 10))
+  TARGET_WS="$((BASE_NUM + 10)):$BASE_NUM"
 else
-  # Otherwise, assume monitor 1 and just use the base number (1-9)
-  TARGET_WS=$BASE_NUM
+  TARGET_WS="$BASE_NUM:$BASE_NUM"
 fi
 
-# Execute the requested i3 action
 if [ "$ACTION" == "switch" ]; then
-  i3-msg workspace number $TARGET_WS
+  i3-msg workspace number "$TARGET_WS"
 elif [ "$ACTION" == "move" ]; then
-  i3-msg move container to workspace number $TARGET_WS
+  i3-msg move container to workspace number "$TARGET_WS"
 fi
